20f86791cfd7d5ee88225dd917f9b3aa899e74a1
adrianblp <adrianblp@gmail.com>
2015-09-06 13:05:49 +0200

    First prototype of title screen

---
+++ Assets/TitleButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems; 
+using UnityEngine.UI;
+
+public class TitleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text buttonText;
+	Color accentColorPrimary = new Color(0.482f, 0.294f, 1.0f);
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		buttonText.color = accentColorPrimary;
+	}
+	
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		buttonText.color = Color.white;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
