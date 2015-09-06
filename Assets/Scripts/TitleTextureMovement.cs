221537cc9ef0c0efc87b402b8b7b6fac31470a5e
adrianblp <adrianblp@gmail.com>
2015-09-06 23:30:13 +0200

    Change title screen block positioning

--- Assets/Scripts/TitleTextureMovement.cs
+++ Assets/Scripts/TitleTextureMovement.cs
@@ -3,22 +3,41 @@ using System.Collections;
 
 public class TitleTextureMovement : MonoBehaviour {
 
+	bool isLeft;
 	float xpos;
 	float offset;
 
 	// Use this for initialization
 	void Start () {
+		// Init whether the block is left or right
+		// This makes it not collide with the buttons
 		xpos = transform.position.x;
+		if (xpos < Screen.width / 2) {
+			isLeft = true;
+		}
+
+		repositionBlock();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.down);
 
-		offset = Random.Range (-200, 200);
-
 		if (transform.position.y < -100) {
-			transform.position = new Vector3(xpos + offset, Screen.height + 50, 0);
+			repositionBlock();
 		}
+	}
+
+	// Puts the block at the top of the screen at the appropriate x coordinate
+	void repositionBlock() {
+		offset = Random.Range (0, (Screen.width / 2) - 300);
+		
+		if (isLeft) {
+			xpos = offset;
+		} else {
+			xpos = Screen.width - offset;
+		}
+
+		transform.position = new Vector3(xpos, Screen.height + 50, 0);
 	}
 }
