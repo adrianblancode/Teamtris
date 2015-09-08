8dcbcc47cfd758e2e7d77d4c1f57466082747026
Victor Hung <vhung@kth.se>
2015-09-08 14:45:40 +0200

    Added function spawnNext() in Spawner which spawns a random tetramino

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,4 +5,19 @@ public class Spawner : MonoBehaviour {
 	
 	// Groups
 	public GameObject[] groups;
+
+	void Start() {
+		// Spawn initial Group
+		spawnNext();
+	}
+
+	public void spawnNext() {
+		// Random Index
+		int i = Random.Range(0, groups.Length);
+		
+		// Spawn Group at current Position
+		Instantiate(groups[i],
+		            transform.position,
+		            Quaternion.identity);
+	}
 }
