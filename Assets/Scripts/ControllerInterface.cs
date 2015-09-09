7e6167a5bbffcd1c170057d72a6172af6f6d96b5
adrianblp <adrianblp@gmail.com>
2015-09-09 16:06:32 +0200

    Refactor ControllerInterface to be futureproof

--- Assets/Scripts/ControllerInterface.cs
+++ Assets/Scripts/ControllerInterface.cs
@@ -16,64 +16,95 @@ public class ControllerInterface : MonoBehaviour {
 	// Call static functions to check status of an input.
 	// The value is stable during the frame and will only 
 	// change between frames
-	//
 
-	//
 	// Player 1 (Player who moves sideways
-	//
 	// team = 1 for team 1
-	// Todo: support for team = 2
 	public static bool MoveRight (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("d"); 
-		return false;
+		if (team == 1) {
+			return Input.GetKey (KeyCode.DownArrow);
+		} else {
+			return Input.GetKey (KeyCode.D);
+		}
 	}
 
 	public static bool MoveLeft (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("a"); 
-		return false;
+		if (team == 1) {
+			return Input.GetKey (KeyCode.LeftArrow); 
+		} else {
+			return Input.GetKey (KeyCode.A);
+		}
 	}
 
-	public static bool MoveButton (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("q"); 
-		return false;
+	// Gives a value between -1 and 1 depending on the tilt of the WiiMote
+	public static float MoveTilt (int team) {
+
+		if (MoveLeft (team)) {
+			return -1;
+		} else if (MoveRight (team)) {
+			return 1;
+		}
+
+		//TODO implement Wiimote stuff
+		return 0;
 	}
 
-
-	//
 	// Player 2 (Player who rotates
-	//
 	// team = 1 for team 1
-	// Todo: support for team = 2
 	public static bool RotRight (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("s"); 
-		return false;
+		if (team == 1) {
+			return Input.GetKey (KeyCode.DownArrow);
+		} else {
+			return Input.GetKey (KeyCode.S);
+		}
 	}
 	
 	public static bool RotLeft (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("w"); 
+		if (team == 1) {
+			return Input.GetKey (KeyCode.UpArrow);
+		} else {
+			return Input.GetKey (KeyCode.W);
+		}
+	}
+
+	// Gives a value between -1 and 1 depending on the tilt of the WiiMote
+	public static float RotTilt (int team) {
+		
+		if (RotLeft (team)) {
+			return -1;
+		} else if (RotRight (team)) {
+			return 1;
+		}
+		
+		//TODO implement Wiimote stuff
+		return 0;
+	}
+
+	/* 
+	 * Action button, this should be the button for menu selection and drop blocks 
+	 */
+
+	// Checks if a specific player on a specific team has pushed the action button
+	// TODO assign wiimote buttons to players
+	public static bool ActionButton (int team, int player) {
+		if (team == 1) {
+			if(player == 1) {
+				return Input.GetKey (KeyCode.Space);
+			} else if (player == 2) {
+				return Input.GetKey (KeyCode.Space);
+			}
+		} else if(team == 2) {
+			if(player == 1) {
+				return Input.GetKey (KeyCode.Space);
+			} else if (player == 2) {
+				return Input.GetKey (KeyCode.Space);
+			}
+		}
+
 		return false;
 	}
-	
-	public static bool RotButton (int team) {
-		if (team == 1)
-			return Input.GetKeyDown("e"); 
-		return false;
-	}
 
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	// Checks whether both players of the team have pressed the action button
+	public static bool ActionButtonCombined (int team) {
+		return ActionButton(team, 1) && ActionButton(team, 2); 
 	}
 }
