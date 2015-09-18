a846e5f54047821e6bb917cb3c8217aeb4b59f0f
Adrian Blanco <adrianblp@gmail.com>
2015-09-18 16:20:01 +0200

    Transforms

--- Assets/Scripts/BlockController1.cs
+++ Assets/Scripts/BlockController1.cs
@@ -190,6 +190,7 @@ public class BlockController1 : MonoBehaviour {
 			StartCoroutine ("Fall");
 		}
 
+		applyTransparency();
 
 	}
 
@@ -474,6 +475,34 @@ public class BlockController1 : MonoBehaviour {
 		}
 	}
 
+	// Applies transparency to all blocks that are behind the current block
+	void applyTransparency() {
+
+		float transparencyZ = 999f;
+
+		foreach (Transform child in currentBlock.transform) {
+			Vector3 temp = new Vector3(child.position.x - gameBoard.transform.position.x, child.position.y - gameBoard.transform.position.y, child.position.z);
+			temp = blockGrid.roundVec3(temp);
+
+			if(temp.z > transparencyZ){
+				transparencyZ = temp.z;
+			}
+		}
+
+		for (int i = 0; i < blockGrid.getDepth(); i++) {
+			Transform[,] grid = blockGrid.getGrid ((int) i);
+
+
+		}
+
+		foreach(Transform child in currentBlock.transform){
+			Color newColor = child.GetComponent<Renderer>().material.color;
+			newColor.a = 0.2f;
+			child.GetComponent<Renderer>().material.color = newColor;
+		}
+
+	}
+	
 	// Displays the new score, level and number of lines to go till next level
 	void updateTexts() {
 		scoreText.text = score.ToString();
