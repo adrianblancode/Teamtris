34abcb4b0ae9daa3860bdb65ef962cc874dd9ccd
ndless <ndless@123>
2015-09-24 20:14:07 +0200

    Forskarfredag tweak.  Keyboard start added
    Supports pressing <SPACE> instead of klicking on teamplay button.
    Info: <ESC> exits game and moves to start screen, but that is an old
    feature.

--- Assets/Scripts/GameLogic/QuitWatcher.cs
+++ Assets/Scripts/GameLogic/QuitWatcher.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class QuitWatcher : MonoBehaviour {
 
+	public SceneLoader sl;
+
 	// If the user presses escape, quit
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			Application.Quit();
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			sl.LoadScene("TeamPlay");
 		}
 	}
 }
