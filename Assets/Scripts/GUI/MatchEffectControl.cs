62a9814201f59085276517622b7c1ecfd5b3ffd1
ndless <ndless@123>
2015-10-19 17:07:43 +0200

    Flashing lights on blocks that soon matches
    Kind of works, but hard to understand what it is and makes it hard to
    see.
    
    Should change the block's apperance instead.

---
+++ Assets/Scripts/GUI/MatchEffectControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchEffectControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("WalkingLights");	
+	}
+	
+	// Update is called once per frame
+	void Update () {			
+	}
+
+	IEnumerator WalkingLights() {
+		while (true) {
+			print("koko  ");
+			foreach (Transform child in transform) {
+				Light spot = child.GetComponent<Light>();
+				spot.enabled = true;
+			}
+			yield return new WaitForSeconds(.3f);
+
+			foreach (Transform child in transform) {
+				Light spot = child.GetComponent<Light>();
+				spot.enabled = false;
+			}
+			yield return new WaitForSeconds(.8f);
+		}
+	}
+}
