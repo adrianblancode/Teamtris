ab3f3d0762762d34a1fa4ae99b66aa6dd1092872
Victor Hung <vhung@kth.se>
2015-10-26 00:24:36 +0100

    Blocks are now generated albeit buggy

--- Assets/Scripts/GameLogic/Block.cs
+++ Assets/Scripts/GameLogic/Block.cs
@@ -1,24 +1,35 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer)]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Block : MonoBehaviour {
 	public int x_size, y_size, z_size;
 
 	private Vector3[] vertices;
 
-	public Block (){
-
+	void Awake(){
+		StartCoroutine ("generateVertices");
 	}
 	
-	private void generateVertices(){
+	IEnumerator generateVertices(){
+		WaitForSeconds wait = new WaitForSeconds (0.05f);
 		vertices = new Vector3[(x_size + 1) * (y_size + 1) * (z_size + 1)];
+		for (int i = 0, z = 0; z <= z_size; ++z) {
+			for(int y = 0; y <= y_size; ++y){
+				for(int x = 0; x <= x_size; ++x){
+					vertices[i] = new Vector3(x,y,z);
+					yield return wait;
+				}
+			}
+		}
 	}
 
 	private void OnDrawGizmos() {
+		Debug.Log ("Hej");
 		if (vertices == null) {
 			return;
 		}
+		Debug.Log ("Drawing");
 		Gizmos.color = Color.black;
 		for (int i = 0; i < vertices.Length; ++i) {
 			Gizmos.DrawSphere(vertices[i], 0.1f);
--- Assets/Scripts/GameLogic/ProcBlock.cs
+++ Assets/Scripts/GameLogic/ProcBlock.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 
 public class ProcBlock : MonoBehaviour {
 
+	private Vector3 block_scale = new Vector3(0.9f, 0.9f, 0.9f);
+
 	private int x_limit, y_limit, z_limit;
 
     public GameObject[] block_parts;
 
-    private enum Directions { Forward=0, Backward, Left, Right, Up, Down };
+    private enum Directions { Forward=0, Backward, Left, Right, Up, Down, Invalid };
     private Dictionary<Directions, Vector3> direction_mapping;
     private Dictionary<Directions, Directions> directions_opposite;
 
@@ -24,17 +26,17 @@ public class ProcBlock : MonoBehaviour {
 
         directions_opposite = new Dictionary<Directions, Directions>();
 
-        directions_opposite.Add(Directions.Right, Directions.Left);
-        directions_opposite.Add(Directions.Left, Directions.Right);
-        directions_opposite.Add(Directions.Up, Directions.Down);
-        directions_opposite.Add(Directions.Down, Directions.Up);
-        directions_opposite.Add(Directions.Forward, Directions.Backward);
-        directions_opposite.Add(Directions.Backward, Directions.Forward);
+        directions_opposite.Add(Directions.Right, 		Directions.Left);
+        directions_opposite.Add(Directions.Left, 		Directions.Right);
+        directions_opposite.Add(Directions.Up, 			Directions.Down);
+        directions_opposite.Add(Directions.Down, 		Directions.Up);
+        directions_opposite.Add(Directions.Forward, 	Directions.Backward);
+        directions_opposite.Add(Directions.Backward, 	Directions.Forward);
     }
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(genBlock());
+		GameObject b = genBlock ();
 	}
 
 	// Update is called once per frame
@@ -43,18 +45,28 @@ public class ProcBlock : MonoBehaviour {
 	}
 
     public GameObject genBlock(){
+		GameObject parent = new GameObject ();
+
         int b = Random.Range(0, block_parts.Length);
-        int num_childs = Random.Range(0, 5);
-        GameObject current = block_parts[b];
-        Directions last_direction;
+        int num_childs = Random.Range(0, 20);
+        
+        Directions last_direction = Directions.Invalid;
+		Vector3 last_position = parent.transform.position;
         for(int i = 0; i < num_childs; ++i){
             Directions dir = (Directions)Random.Range(0, 6);
+			while(last_direction != Directions.Invalid && dir != directions_opposite[last_direction]){
+				dir = (Directions)Random.Range(0, 6);
+			}
+			Vector3 new_position = last_position + direction_mapping[dir];
+			GameObject current = (GameObject)Instantiate(block_parts[b], new_position, parent.transform.rotation);
 
+			current.transform.localScale = block_scale;
+			current.transform.SetParent(parent.transform);
 
-
+			last_position = new_position;
             last_direction = dir;
         }
 
-        return current;
+        return parent;
     }
 }
