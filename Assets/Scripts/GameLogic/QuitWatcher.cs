112c704ae7b8be531c6e4f78034521d8c9f69fe4
ndless <ndless@123>
2015-10-27 19:23:19 +0100

    Keyboard start added
    Inherited from the Forskarfredag setup

--- Assets/Scripts/GameLogic/QuitWatcher.cs
+++ Assets/Scripts/GameLogic/QuitWatcher.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class QuitWatcher : MonoBehaviour {
 
+	public SceneLoader sl;
+
 	// If the user presses escape, quit
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			Application.Quit();
+		}
+
+		// Start game if user press <SPACE>
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			sl.LoadScene("TeamPlay");
 		}
 	}
 }
